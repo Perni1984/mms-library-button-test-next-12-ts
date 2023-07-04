f97d9479b4143c100b70a045d5c0aa01bc7c5935
pernsteinerc <pc@pcode.at>
2023-07-04 16:06:44 +0200

    feat: add cards game to next.js app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { CardProductGrids } from "@mms/webmobile-sc-components";
+import { ModularCardProduct } from "../src/CardProduct.mock";
+import GridLayout from "react-grid-layout";
 
 const Home: NextPage = () => {
+  const layout = [
+    { i: "a", x: 0, y: 0, w: 1, h: 1 },
+    { i: "b", x: 1, y: 0, w: 1, h: 1 },
+    { i: "c", x: 2, y: 0, w: 1, h: 1 },
+  ];
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,61 +20,73 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
+      <header className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Playing Cards with Product Cards in a Next.js App
         </a>
-      </footer>
+      </header>
+      <main>
+        <GridLayout className="layout" layout={layout} cols={4} rowHeight={660}>
+          <div key="a">
+            {" "}
+            {ModularCardProduct(
+              CardProductGrids.COLUMN,
+              true,
+              "Product 1",
+              undefined,
+              true,
+              true,
+              true,
+              true,
+              false,
+              true,
+              false,
+              true,
+              true
+            )}
+          </div>
+          <div key="b">
+            {ModularCardProduct(
+              CardProductGrids.COLUMN,
+              true,
+              "Product 2",
+              undefined,
+              false,
+              true,
+              true,
+              true,
+              false,
+              true,
+              true,
+              true,
+              true
+            )}
+          </div>
+          <div key="c">
+            {ModularCardProduct(
+              CardProductGrids.COLUMN,
+              true,
+              "Product 3",
+              undefined,
+              false,
+              true,
+              true,
+              true,
+              false,
+              true,
+              true,
+              true,
+              true
+            )}
+          </div>
+        </GridLayout>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
